7edb15c88ceb1a6ff659148641a32b911c44e0c0
John <orkun.e@hotmail.com>
2024-02-07 14:59:53 -0500

    24.gun

--- src/test/java/pages/WebuniversityPage.java
+++ src/test/java/pages/WebuniversityPage.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
-public class WebUniversityPage {
+public class WebuniversityPage {
 
-    public WebUniversityPage(){
+    public WebuniversityPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
@@ -22,7 +22,4 @@ public class WebUniversityPage {
 
     @FindBy(xpath = "//button[@id='login-button']")
     public WebElement loginButonu;
-
-
-
 }
